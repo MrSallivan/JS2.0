7e73a4b01b58a0d78b13b078c6e5ca1933a1476d
MrSallivan <nik.sallivan@gmail.com>
2020-07-08 23:52:56 +1000

    1

--- lesson6(cycle2)/unit_06/unit_06.js
+++ lesson6(cycle2)/unit_06/unit_06.js
@@ -6,7 +6,15 @@
 // <p>где звездочки рисуются с помощью внутреннего цикла от 0 до 3, а _ с помощью внешнего.</p>
 
 function t1() {
-
+	let out = ''
+	let outTask = document.querySelector('.out-1')
+	for (let i = 0; i < 3; i++) {
+		for (let k = 0; k < 3; k++) {
+			out += '*'
+		}
+		out += '_'
+	}
+	outTask.innerHTML = out
 }
 
 document.querySelector('.b-1').onclick = t1;
