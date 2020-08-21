5a87b947d18775cd3e4b96888eef6be67fdf8819
MrSallivan <nik.sallivan@gmail.com>
2020-08-21 16:21:54 +1000

    14

---
+++ lesson14(practiks_API_radio)/project_api_14/js/script.js
@@ -0,0 +1,11 @@
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Khabarovsk,ru&appid=d0db13a6c2cef9cd83e17d46c677c948')
+	.then(function (resp) { return resp.json() })
+	.then(function (data) {
+		console.log(data)
+		document.querySelector('.weather__cityname').textContent = data.name
+		document.querySelector('.weather__deg').innerHTML = Math.round(data.main.temp - 273) + '&deg;'
+		document.querySelector('.weather__text').textContent = data.weather[0].description
+		document.querySelector('.cloud').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
+		document.querySelector('.wings').innerHTML = ` Ветер: ${data.wind.speed} м/с`
+
+	})
