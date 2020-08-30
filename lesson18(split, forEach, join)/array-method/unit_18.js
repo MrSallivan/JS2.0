ad570e5ec2caee614419b6ba29cb029c2e45c384
MrSallivan <nik.sallivan@gmail.com>
2020-08-30 23:08:17 +1000

    18.10

--- lesson18(split, forEach, join)/array-method/unit_18.js
+++ lesson18(split, forEach, join)/array-method/unit_18.js
@@ -131,7 +131,19 @@ document.querySelector('.b-9').onclick = t9;
 /*  Дан массив a10 = {name: ivan, age: 15, sex: 1, id: 45} - преобразуйте его в GET строку (GET параметры). Найдите описание что такое GET строка самостоятельно. Разделитель - амперсанд. Результат присвойте a10_res. Запускаться решение должно при вызове функции t10. Допускается лишний амперсанд в конце строки!!! */
 
 function t10() {
+	a10 = {
+		name: 'ivan',
+		age: 15,
+		sex: 1,
+		id: 45
+	}
+	a10_res = 'https://site.ru?'
+	for (let key in a10) {
 
+		a10_res += `${key}=${a10[key]}&`
+
+	}
+	console.log(a10_res)
 }
 
 document.querySelector('.b-10').onclick = t10;
