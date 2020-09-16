e40ab6da1b5f1e8625dac9852279ae6fd236bb70
MrSallivan <nik.sallivan@gmail.com>
2020-09-16 16:16:32 +1000

    22.5

--- lesson22(try, catch)/app/script.js
+++ lesson22(try, catch)/app/script.js
@@ -1,3 +1,20 @@
 let a =5
-a = a*2
-document.querySelector('.test').innerHTML = a
+a = a * 2
+
+try {
+	document.querySelector('.test').innerHTML = a	
+}
+catch(err) {
+	console.log(err)
+	console.log('1')
+}
+finally {
+	alert('App work!!')
+}
+
+console.log(a)
+t1()
+
+function t1() {
+	console.log('hello')
+}
