2e369a2190984ca453ff5ed49d17c08887d36d29
MrSallivan <nik.sallivan@gmail.com>
2020-09-19 18:54:57 +1000

    23.33

--- lesson23(local_storahe)/dz_23_blank/unit_23.js
+++ lesson23(local_storahe)/dz_23_blank/unit_23.js
@@ -29,8 +29,7 @@ function t3() {
 		out += i + ' ' + b[i] + '<br>'
 	}
 	document.querySelector('.out-3').innerHTML = out
-	console.log(b)
-	console.log(typeof (b))
+
 }
 
 
@@ -41,38 +40,49 @@ document.querySelector('.b-3').onclick = t3
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
 
 function t4() {
-
+	a4 = { hello: 'world', hi: 'mahai' }
+	localStorage.setItem('a4', JSON.stringify(a4))
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-4').onclick = t4
 
 // Task 5 ============================================
 /*   При нажатии кнопки b-5 выведите из LS сохранненный массив a4. Выведите в out-5 в формате ключ пробел значение перенос строки. */
 
 function t5() {
-
+	let b = JSON.parse(localStorage.getItem('a4'))
+	let out = ''
+	for (let index in b) {
+		out += index + ' ' + b.index + '<br>'
+	}
+	document.querySelector('.out-5').innerHTML = out
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-5').onclick = t5
 
 // Task 6 ============================================
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
 
 function t6() {
-
+	localStorage.clear()
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-6').onclick = t6
 
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
 
+a7 = []
+
 function t7() {
+	let input = document.querySelector('.i-7').value
+	a7.push(input)
+	localStorage.setItem
 
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-7').onclick = t7
 
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
